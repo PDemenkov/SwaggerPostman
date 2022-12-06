2bc04e7cb5b0a99e9c7c9988e7e1097d3ee95950
PDemenkov <guapnik@gmail.com>
2022-12-06 17:54:13 +0300

    Initial commit

---
+++ src/test/java/com/example/swaggerpostman/SwaggerPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swaggerpostman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerPostmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
